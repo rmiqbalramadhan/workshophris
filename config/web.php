451850c735296f35d413c52bfd2c118cb658603d
rmiqbalramadhan <rmiqbalramadhan@gmail.com>
2021-02-01 19:01:55 +0700

    change to id language

--- config/web.php
+++ config/web.php
@@ -5,6 +5,8 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'language' => 'id-ID',
+    'sourceLanguage' => 'id-ID',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'modules' => [
